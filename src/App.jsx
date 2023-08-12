8c4d0376d824ecc18bb3a7924379bf5047f657a9
IsmayelAlam <ismayelalam0@gmail.com>
2023-08-12 12:47:00 +0600

    add signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Login from "./component/pages/Login";
+import SignUp from "./component/pages/SignUp";
 
 function App() {
   return (
     <>
       <Login />
+      <SignUp />
     </>
   );
 }
--- src/component/pages/SignUp.jsx
+++ src/component/pages/SignUp.jsx
@@ -1,12 +1,12 @@
 import { FaFacebookSquare, FaGoogle, FaTwitter } from "react-icons/fa";
 
-export default function Login() {
+export default function SignUp() {
   return (
     <div className="authPage">
       <div className="authBox">
         <img src="/logo.svg" alt="" />
         <div className="authLink">
-          <p>Login with</p>
+          <p>Sign up with</p>
           <div className="links">
             <FaGoogle />
             <FaFacebookSquare />
@@ -20,22 +20,24 @@ export default function Login() {
         </div>
         <form className="authFrom">
           <input type="text" id="username" placeholder="username" required />
-
+          <input type="email" id="email" placeholder="email" required />
           <input
             type="password"
             id="password"
             placeholder="password"
             required
           />
+          <input
+            type="password"
+            id="password"
+            placeholder="confirm password"
+            required
+          />
 
-          <button type="submit">login</button>
+          <button type="submit">sign up</button>
         </form>
         <p className="textLink">
-          forget password? <a href="#">recover</a>
-        </p>
-        <p className="textLink">
-          create a new account
-          <a href="#"> &rarr;</a>
+          already have an account? <a href="#">login</a>
         </p>
       </div>
     </div>
